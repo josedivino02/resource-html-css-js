2dd903ae21c59ab447a9ab15f1beb7ec08721408
José Divino <josedivinooficial02@gmail.com>
2023-06-26 14:36:36 -0300

    ScrollSuave transform i class

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,29 +1,37 @@
-export default function initScrollSuave() {
-  const linksInternos = document.querySelectorAll(
-    '[data-menu="suave"] a[href^="#"]',
-  );
+export default class ScrollSuave {
+  constructor(links, options) {
+    this.linksInternos = document.querySelectorAll(links);
 
-  if (linksInternos) {
-    function scrollToSection(event) {
-      event.preventDefault();
-      const href = event.currentTarget.getAttribute('href');
-      const section = document.querySelector(href);
-
-      section.scrollIntoView({
+    if (options === undefined) {
+      this.options = {
         behavior: 'smooth',
         block: 'start',
-      });
-
-      // const topo = section.offsetTop;
-
-      // scrollTo({
-      //   top: topo,
-      //   behavior: 'smooth',
-      // });
+      };
+    } else {
+      this.options = options;
     }
 
-    linksInternos.forEach(link => {
-      link.addEventListener('click', scrollToSection);
+    this.scrollToSection = this.scrollToSection.bind(this);
+  }
+
+  scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+
+    section.scrollIntoView(this.options);
+  }
+
+  addLinkEvent() {
+    this.linksInternos.forEach(link => {
+      link.addEventListener('click', this.scrollToSection);
     });
+  }
+
+  init() {
+    if (this.linksInternos.length) {
+      this.addLinkEvent();
+    }
+    return this;
   }
 }
